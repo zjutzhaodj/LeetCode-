86f7bdf87ba7d9c3de6bbbfe484a7516d6fbe9c8
fakerzhao@dingtone.me <fakerzhao@dingtone.me>
2018-05-02 20:56:25 +0800

    create 226反转二叉树

--- src/TreeTest.java
+++ src/TreeTest.java
@@ -6,39 +6,60 @@
 public class TreeTest {
 
     public static void main(String args[]) {
-        TreeNode node =new TreeNode(3,new TreeNode(9,null,null),new TreeNode(20,new TreeNode(15,null,null),new TreeNode(7,null,null)));
+        TreeNode node = new TreeNode(3, new TreeNode(9, null, null), new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null)));
         System.out.print(maxDepth(node));
     }
 
 
     public static int maxDepth(TreeNode root) {
 
-        if(root == null){
+        if (root == null) {
             return 0;
         }
-        return Math.max(maxDepth(root.left)+1,maxDepth(root.right)+1);
+        return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
 
     }
 
 
-
-
-
     public static TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-            if(t1==null && t2 ==null){
-                return null;
-            }
-            if(t1!=null && t2!=null){
-                t1.val = t1.val+t2.val;
-            }else if(t1==null && t2!=null){
-                return t2;
-            }else if(t1!=null && t2 ==null){
-                return t1;
-            }
-            t1.left = mergeTrees(t1.left,t2.left);
-            t1.right = mergeTrees(t1.right,t2.right);
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        if (t1 != null && t2 != null) {
+            t1.val = t1.val + t2.val;
+        } else if (t1 == null && t2 != null) {
+            return t2;
+        } else if (t1 != null && t2 == null) {
             return t1;
+        }
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
 
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+
+
+        if (root == null) {
+            return root;
+        }
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+
+
+//        if (root == null) {
+//            return null;
+//        }
+//        TreeNode right = invertTree(root.right);
+//        TreeNode left = invertTree(root.left);
+//        root.left = right;
+//        root.right = left;
+//        return root;
     }
 
 
